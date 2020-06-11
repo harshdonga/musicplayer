b9b62612f112fe6dba5b3a218b470dd7a5ade84e
harshdonga <harshdonga99@gmail.com>
2020-06-12 05:25:51 +0530

    Working Commit 3

--- musicplayer/settings.py
+++ musicplayer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '84ej9pdlg_zo6(1p3)=c_w3#4#a-%kemo1bvbn2ikfxujg037)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
